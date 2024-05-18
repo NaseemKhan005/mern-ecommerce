0936de29cf32e07665a8affeda9b21d28f984bac
Naseem Khan <naseemsaffy@gmail.com>
2024-05-18 12:39:17 +0500

    feat: Add ProductModel for managing product data

---
+++ server/src/models/ProductModel.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+    },
+    size: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    inStock: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
