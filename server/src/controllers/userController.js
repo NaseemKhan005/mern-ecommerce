8718b2e97eb471d73616a4f605f866e3b9c755f2
Naseem Khan <naseemsaffy@gmail.com>
2024-05-18 17:43:51 +0500

    feat: Add getting all users and single user functionality

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -37,3 +37,34 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getSingleUser = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+
+    const user = await User.findById(userId);
+    if (!user) return next(createError(404, "User not found"));
+
+    const { password, ...userInfo } = user._doc;
+
+    res.status(200).json({
+      message: "Single user fetched successfully",
+      user: userInfo,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find().select("-password");
+
+    res.status(200).json({
+      message: "All users fetched successfully",
+      users: users,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { updateUser, deleteUser } from "../controllers/userController.js";
+import {
+  updateUser,
+  deleteUser,
+  getSingleUser,
+  getAllUsers,
+} from "../controllers/userController.js";
 import verifyUser from "../middlewares/verifyUser.js";
+import verifyAdmin from "../middlewares/verifyAdmin.js";
 const router = express.Router();
 
 router.put("/:id", verifyUser, updateUser);
 router.delete("/:id", verifyUser, deleteUser);
+router.get("/user/:id", verifyAdmin, getSingleUser);
+router.get("/", verifyAdmin, getAllUsers);
 
 export default router;
