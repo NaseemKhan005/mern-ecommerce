8d02042c0ed8736551efe4f3d8f1c28b099fcbe1
Naseem Khan <naseemsaffy@gmail.com>
2024-05-19 15:11:53 +0500

    feat: Add Create new product controller

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -1,6 +1,12 @@
+import Product from "../models/ProductModel.js";
+
 export const createNewProduct = async (req, res, next) => {
   try {
-    res.status(201).json({ message: "Product created Successfully!" });
+    const product = await Product.create(req.body);
+
+    res
+      .status(201)
+      .json({ message: "Product created Successfully!", product: product });
   } catch (error) {
     next(error);
   }
--- server/src/routes/productRoute.js
+++ server/src/routes/productRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { createNewProduct } from "../controllers/productController.js";
+import verifyAdmin from "../middlewares/verifyAdmin.js";
 const router = express.Router();
 
-router.post("/", createNewProduct);
+router.post("/", verifyAdmin, createNewProduct);
 
 export default router;
